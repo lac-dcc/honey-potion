42235caafceb393327f82956dcaa068f9b7545bb
Thais Damasio <thais.damasio@outlook.com.br>
2022-05-30 03:10:45 -0300

    Adding a tip

--- benchmark/programs/countsyscalls/prog.h
+++ benchmark/programs/countsyscalls/prog.h
@@ -1,6 +1,12 @@
 #define NSYS 3
 struct syscallNames { long id; char name[15]; };
 
+/**
+ * @brief If you want, you can list all the syscalls
+ * using the following command line:
+ * `ausyscall --dump`
+ * You can add a new syscall in this struct if you want
+ */
 struct syscallNames SYSCALLSNAMES[NSYS] = {
     {62, "enter_kill"}, 
     {83, "enter_mkdir"},
