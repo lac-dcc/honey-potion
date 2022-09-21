6c14894ed85cfbf7f1d31ea32176e51f9eef3179
Kael Augusto <kael10@mail.com>
2022-09-21 13:51:03 -0300

    Removed debug prints

--- lib/honey/variable_analysis.ex
+++ lib/honey/variable_analysis.ex
@@ -1,5 +1,6 @@
 defmodule Honey.Analyze do
   def run(ast) do
+
     #Does a postwalk to count how many uses for each variable. First element of tuple is ignored.
     {_,lvu} = Macro.postwalk(ast, [] , fn segment, lvu ->
       if match?({_, [version: _, line: _], nil}, segment) do
@@ -11,18 +12,14 @@ defmodule Honey.Analyze do
       end
     end)
 
-    IO.inspect(lvu)
-
     #Does a second postwalk to recount, if the new count reached the first one, that's the last use.
     {ast, _} = Macro.postwalk(ast, [] , fn segment, nlvu ->
       if match?({_, [version: _, line: _], nil}, segment) do
         {var, [version: v, line: l], nil} = segment
         {_,nlvu} = Keyword.get_and_update(nlvu, var, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
         if(Keyword.get(nlvu, var) == Keyword.get(lvu, var)) do
-          IO.puts("Variable " <> to_string(var) <> " was last used in line " <> to_string(l))
-
+          #Prints the variable and the line of the last use:: #IO.puts("Variable " <> to_string(var) <> " was last used in line " <> to_string(l))
           segment = {var, [version: v, line: l, last: true], nil}
-
           {segment, nlvu}
         else
           segment = {var, [version: v, line: l, last: false], nil}
@@ -36,7 +33,6 @@ defmodule Honey.Analyze do
     end)
 
     ast
+
   end
-
 end
-
