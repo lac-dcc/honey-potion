2f3074ea5e0f17056e9b795eace58f7b57868a7d
Kael Augusto <kael10@mail.com>
2022-09-22 12:40:50 -0300

    Using is_var and tuples for analysis

--- lib/honey/variable_analysis.ex
+++ lib/honey/variable_analysis.ex
@@ -1,10 +1,11 @@
 defmodule Honey.Analyze do
   def run(ast) do
-
+    import Honey.Utils, only: [is_var: 1]
     #Does a postwalk to count how many uses for each variable. First element of tuple is ignored.
+    #lvu == List of Variable Uses
     {_,lvu} = Macro.postwalk(ast, [] , fn segment, lvu ->
-      if match?({_, [version: _, line: _], nil}, segment) do
-        {var, [version: _, line: _], nil} = segment
+      if is_var(segment) do
+        var = elem(segment, 0)
         {_,lvu} = Keyword.get_and_update(lvu, var, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
         {:ok, lvu}
       else
@@ -14,15 +15,17 @@ defmodule Honey.Analyze do
 
     #Does a second postwalk to recount, if the new count reached the first one, that's the last use.
     {ast, _} = Macro.postwalk(ast, [] , fn segment, nlvu ->
-      if match?({_, [version: _, line: _], nil}, segment) do
-        {var, [version: v, line: l], nil} = segment
+      if is_var(segment) do
+        var = elem(segment, 0)
+        meta = elem(segment, 1)
+        context = elem(segment, 2)
         {_,nlvu} = Keyword.get_and_update(nlvu, var, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
         if(Keyword.get(nlvu, var) == Keyword.get(lvu, var)) do
           #Prints the variable and the line of the last use:: #IO.puts("Variable " <> to_string(var) <> " was last used in line " <> to_string(l))
-          segment = {var, [version: v, line: l, last: true], nil}
+          segment = {var, meta ++ [{:last, false}], context}
           {segment, nlvu}
         else
-          segment = {var, [version: v, line: l, last: false], nil}
+          segment = {var, meta ++ [{:last, false}], context}
 
           {segment, nlvu}
         end
