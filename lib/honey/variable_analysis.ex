3d2a07a8553ac833f380afed71358b1f73a3d42b
Kael Augusto <kael10@mail.com>
2022-09-28 16:39:58 -0300

    Static analysis now considers version and context

--- lib/honey/variable_analysis.ex
+++ lib/honey/variable_analysis.ex
@@ -1,12 +1,21 @@
 defmodule Honey.Analyze do
   def run(ast) do
     import Honey.Utils, only: [is_var: 1]
+
     #Does a postwalk to count how many uses for each variable. First element of tuple is ignored.
     #lvu == List of Variable Uses
     {_,lvu} = Macro.postwalk(ast, [] , fn segment, lvu ->
       if is_var(segment) do
-        var = elem(segment, 0)
-        {_,lvu} = Keyword.get_and_update(lvu, var, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
+        #Get info from segment
+        {var, meta, context} = segment
+        ver = Keyword.get(meta, :version)
+
+        #Transform info to unique key
+        keystring = to_string(var) <> "_" <> to_string(ver) <> "_" <> to_string(context)
+        keyatom = String.to_atom(keystring)
+
+        #Keep key and number of uses in Keyword
+        {_,lvu} = Keyword.get_and_update(lvu, keyatom, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
         {:ok, lvu}
       else
         {:none, lvu}
@@ -16,16 +25,24 @@ defmodule Honey.Analyze do
     #Does a second postwalk to recount, if the new count reached the first one, that's the last use.
     {ast, _} = Macro.postwalk(ast, [] , fn segment, nlvu ->
       if is_var(segment) do
-        var = elem(segment, 0)
-        meta = elem(segment, 1)
-        context = elem(segment, 2)
-        {_,nlvu} = Keyword.get_and_update(nlvu, var, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
-        if(Keyword.get(nlvu, var) == Keyword.get(lvu, var)) do
-          #Prints the variable and the line of the last use:: #IO.puts("Variable " <> to_string(var) <> " was last used in line " <> to_string(l))
-          segment = {var, meta ++ [{:last, true}], context}
+        #Get info from segment
+        {var, meta, context} = segment
+        ver = Keyword.get(meta, :version)
+
+        #Transform info to unique key
+        keystring = to_string(var) <> "_" <> to_string(ver) <> "_" <> to_string(context)
+        keyatom = String.to_atom(keystring)
+
+        #Keep key and number of uses in Keyword
+        {_,nlvu} = Keyword.get_and_update(nlvu, keyatom, fn x -> if is_integer(x), do: {x, x + 1}, else: {nil, 1} end)
+
+        #Confirm if this use is the last use. Adds :last accordingly [true or false].
+        if(Keyword.get(nlvu, keyatom) == Keyword.get(lvu, keyatom)) do
+          segment = {var, Keyword.put(meta, :last, true), context}
+
           {segment, nlvu}
         else
-          segment = {var, meta ++ [{:last, false}], context}
+          segment = {var, Keyword.put(meta, :last, false), context}
 
           {segment, nlvu}
         end
