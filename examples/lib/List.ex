7626afc40a5fd05de2aff5ea1b8905ebf5ccf542
Vinícius Julião R <vrjuliao@hotmail.com>
2022-12-01 16:06:51 -0300

    Adding examples

---
+++ examples/lib/List.ex
@@ -0,0 +1,13 @@
+defmodule List do
+  use Honey, license: "Dual BSD/GPL"
+
+  @sec "tracepoint/syscalls/sys_enter_kill"
+  def main(_) do
+    one_two = {1, 2}
+    three = 3
+    [_, any_tuple] = [three, one_two]
+    {x, _} = any_tuple
+    # X must be equals 1
+    Honey.Bpf.Bpf_helpers.bpf_printk(["x: %d", x])
+  end
+end
