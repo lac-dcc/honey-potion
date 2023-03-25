3a71963f77b4dea10f25f7a6c2456d54f9236e97
Kael Soares Augusto <kaelsa@hotmail.com.br>
2023-03-24 21:09:52 -0300

    Added new Namespace to bpf.ex to allow IDE autocomplete.

--- lib/honey/c_libraries/bpf.ex
+++ lib/honey/c_libraries/bpf.ex
@@ -1,15 +1,13 @@
-defmodule Honey.Bpf do
-  defmodule Bpf_helpers do
-    def bpf_printk(_str) do
-    end
+defmodule Honey.Bpf_helpers do
+  def bpf_printk(_str) do
+  end
 
-    def bpf_get_current_pid_tgid() do
-    end
+  def bpf_get_current_pid_tgid() do
+  end
 
-    def bpf_map_lookup_elem(_map, _key) do
-    end
+  def bpf_map_lookup_elem(_map, _key) do
+  end
 
-    def bpf_map_update_elem(_map, _key, _value) do
-    end
+  def bpf_map_update_elem(_map, _key, _value) do
   end
 end
