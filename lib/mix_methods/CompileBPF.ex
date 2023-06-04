ef1b788df6e833980f6e9842680ec4a5b28e5fd5
Kael Soares Augusto <kaelsa@hotmail.com.br>
2023-06-04 17:51:16 -0300

    Removed Libbpf from within Honey-Potion.

--- lib/mix_methods/CompileBPF.ex
+++ lib/mix_methods/CompileBPF.ex
@@ -2,7 +2,9 @@ defmodule CompileBPF do
   use Mix.Task
 
   def run(_) do
-    path = Path.join(File.cwd!(), "benchmarks/libs/libbpf/src/")
-    System.cmd("make", [], cd: path)
+    # This was used when we compiled libbpf ourselves. Now it is not needed.
+    # Left here in case it's needed again as a reference.
+    #path = Path.join(File.cwd!(), "benchmarks/libs/libbpf/src/")
+    #System.cmd("make", [], cd: path)
   end
 end
