40be4150546f64d5ec9f137a5f3144a9cebf22a0
Kael Soares Augusto <kaelsa@hotmail.com.br>
2023-11-07 15:51:16 -0300

    Added 3 new examples. CP, DCE and Fact.

---
+++ examples/lib/DCE.ex
@@ -0,0 +1,13 @@
+defmodule DCE do
+  use Honey, license: "Dual BSD/GPL"
+
+  @sec "tracepoint/syscalls/sys_enter_kill"
+  def main(_ctx) do
+    if true do
+      1
+    else
+      Honey.Bpf_helpers.bpf_printk([["This will never print!"]])
+      Honey.Bpf_helpers.bpf_printk([["This will also not be in the AST after DCE!"]])
+    end
+  end
+end
