447a4bc7bd0222fc0265a29447fbff3df861f958
Fernando Quintao Pereira <pronesto@gmail.com>
2024-08-15 11:24:13 -0300

    Adding RQ2's summary

--- artifact/rq2.sh
+++ artifact/rq2.sh
@@ -15,8 +15,16 @@
 #
 ##############################################################################
 
-echo "This script reproduces Figure 14 of the paper:"
-echo "Honey Potion: an eBPF Backend for Elixir"
+# Print summary of results:
+#
+echo ""
+echo "########################################"
+echo "Data for Figure 14"
+echo "########################################"
+echo ""
+echo " * hand_written_size: size (bytes) of text segment of hand written C apps"
+echo " * honey_potion_size: size (bytes) of text segment of Honey Potion apps"
+echo ""
 
 
 # This function gets the size of the text segment of binary programs.
@@ -26,7 +34,7 @@ get_text_segment_size() {
   local text_size=$(size "$file" | awk 'NR==2 {print $1}')
   echo "$text_size"
 }
-  
+
 
 # Move to the folder where the C files are located, compile them and count LoC:
 #
@@ -37,12 +45,12 @@ script_dir=$(cd "$(dirname "$0")" && pwd)
 hw_sizes=()
 for folder in "${folders[@]}"
 do
-        full_path="$bench_path/${folder}"
-        cd $full_path
-        make
-        bin_size=$(get_text_segment_size ./prog)
-        hw_sizes+=("$bin_size")
-        cd $script_dir
+	full_path="$bench_path/${folder}"
+	cd $full_path
+	make  >/dev/null 2>&1
+	bin_size=$(get_text_segment_size ./prog)
+	hw_sizes+=("$bin_size")
+	cd $script_dir >/dev/null
 done
 
 
@@ -54,20 +62,19 @@ bench_path="../examples"
 
 cd $bench_path
 mix deps.get
-mix compile --force
-cd -
+mix compile --force >/dev/null 2>&1
+cd - >/dev/null
 
 hp_sizes=()
 for folder in "${folders[@]}"
 do
-        full_path="$bench_path/lib/bin/${folder}"
-        bin_size=$(get_text_segment_size $full_path)
-        hp_sizes+=("$bin_size")
-        cd $script_dir
+	full_path="$bench_path/lib/bin/${folder}"
+	bin_size=$(get_text_segment_size $full_path)
+	hp_sizes+=("$bin_size")
+	cd $script_dir
 done
 
 # Print the hp_sizes
-echo ""
 echo "Benchmark, hand_written_size, honey_potion_size"
 for ((i=0; i<${#hp_sizes[@]}; i++)); do
   echo "${folders[$i]}, ${hw_sizes[$i]}, ${hp_sizes[$i]}"
