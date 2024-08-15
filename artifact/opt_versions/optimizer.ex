fc3ef72731444beb3bbd4ac77b955ee173f706aa
Fernando Quintao Pereira <pronesto@gmail.com>
2024-08-15 11:05:50 -0300

    Adding material to reproduce RQ4

---
+++ artifact/opt_versions/optimizer.ex
@@ -0,0 +1,20 @@
+defmodule Honey.Optimizer do
+  alias Honey.{ConstantPropagation, DCE, Analyze, TypePropagation}
+
+  @moduledoc """
+  Module to define and run the optimization pipeline that runs over elixirs AST.
+  """
+
+  @doc """
+  Runs the optimization and analysis steps.
+  """
+
+  def run(fun_def, arguments, env) do
+    fun_def |> Analyze.run()
+    |> ConstantPropagation.run()
+    |> DCE.run()
+    |> TypePropagation.run(arguments, env)
+    # |> AstSize.output(env, " - Final")
+    # |> IO.inspect()
+  end
+end
---
+++ artifact/rq4.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+##############################################################################
+#
+# This script reproduces RQ4: Optimizations. The numbers produced should be
+# used to rebuild Figure 16 in the paper:
+# "Honey Potion: an eBPF Backend for Elixir".
+#
+# The script only dumps text. It measures the size of the ASTs produced for
+# different Honey Potion programs, using different groups of optimizations.
+#
+# Author: Fernando Magno Quintao Pereira
+# August 15th, 2024
+#
+##############################################################################
+
+echo "This script reproduces Figure 16 of the paper:"
+echo "Honey Potion: an eBPF Backend for Elixir"
+
+
+# This function extracts the size of the AST, as logged by the Honey Potion
+# compiler:
+#
+extract_ast_size() {
+  local log_file="$1"
+  local search_string="$2"
+  grep "$search_string" "$log_file" | awk -F' - ' '{print $2}'
+}
+
+
+# Move to the folder where the Elixir files are located, compile them and
+# record the sizes of the ASTs:
+#
+optimizers=("noOptimization" "typeProp" "constProp_TypeProp" "constProp_DeadCodeElim_TypeProp")
+bench_path="../examples"
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+for optimizer in "${optimizers[@]}"
+do
+	cp "opt_versions/$optimizer" "../lib/honey/optimizer.ex"
+	
+	# Compile all the programs in the examples folder, using the new
+	# optimizer:
+	#
+	cd $bench_path
+	mix deps.get
+	mix compile --force > "$script_dir/$optimizer.txt"
+	cd -
+
+	# Put back the original optimizer into the honey library:
+	#
+	cp "opt_versions/optimizer.ex" "../lib/honey/optimizer.ex"
+done
+
+
+# These are the current benchmarks that we have in the examples folder:
+#
+folders=("Case" "Cond" "CountSysCalls" "CP" "CtxAccess" "DCE" "DropUdp" "Forcekill" "GetPID" "HelloWorld" "Honey_Fact" "Honey_List" "Honey_List_Linked" "Honey_Maps" "Honey_Tuple" "If_Then_Else" "Integer_String_Pattern_Matching" "ListSysCalls" "One" "TrafficCount")
+
+# Print summary of results:
+#
+echo ""
+echo ""
+echo "########################################"
+echo "Data for Figure 16"
+echo "########################################"
+echo ""
+echo " * noOptimization: all the optimizations disabled"
+echo " * typeProp: only type propagation (equivalent to no optimization)"
+echo " * constProp_TypeProp: type propagation and constant propagation"
+echo " * constProp_TypeProp_DeadCodeElim: type propagation, constant propagation and dead-code elimination"
+
+# Print the table header:
+#
+echo ""
+echo -n "Benchmark, "
+for optimizer in "${optimizers[@]}"
+do
+	echo -n "$optimizer, "
+done
+echo ""
+
+# Print the table body:
+#
+for folder in "${folders[@]}"
+do
+
+	echo -n "$folder, "
+
+	for optimizer in "${optimizers[@]}"
+	do
+		ast_size=$(extract_ast_size "$optimizer.txt" "$folder ")
+        		echo -n "$ast_size, "
+	done
+	echo ""
+
+done
+
+# Remove log files:
+#
+for optimizer in "${optimizers[@]}"
+do
+	rm -rf "$optimizer.txt"
+done
