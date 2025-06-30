4aa3f42d0db9862a6ce9e9c5d6d0bff6da153fc8
Kael Soares Augusto <kaelsa@hotmail.com.br>
2025-06-29 22:21:45 -0300

    First implementation with defragmentation. Lacks position reassignment after defragmentation.

--- lib/honey/compiler/translator.ex
+++ lib/honey/compiler/translator.ex
@@ -69,25 +69,83 @@ defmodule Honey.Compiler.Translator do
       {pos, _, _} -> pos
     end
 
-    if TypeSet.is_generic?(translated_code.return_var_type) do
-      "(*(Generic*) (stack + #{c_var_position}))"
-    else
-      if TypeSet.is_integer?(translated_code.return_var_type) do
-      "(*(int*) (stack + #{c_var_position}))"
-      else
-        # TODO: This should handle CTX variables somehow. Currently (?) they get a type of :type_ctx_pid.
-      "(*(Generic*) (stack + #{c_var_position}))"
-      end
+    cond do
+      TypeSet.is_generic?(translated_code.return_var_type) ->
+        "(*(Generic*) (stack + #{c_var_position}))"
+      TypeSet.is_integer?(translated_code.return_var_type) ->
+        "(*(int*) (stack + #{c_var_position}))"
+      TypeSet.is_void?(translated_code.return_var_type) ->
+        "(*(void**) ((stack + #{c_var_position})))"
+      true ->
+      # TODO: This should handle CTX variables somehow. Currently (?) they get a type of :type_ctx_pid.
+        "(*(Generic*) (stack + #{c_var_position}))"
     end
+  end
+
+  def get_var_stack_pos(context, var_name) do
+    elem = Map.get(context.var_pointer_map, var_name)
+    case elem do
+      {pos, _} -> pos
+      {pos, _, _} -> pos
+    end
+  end
+
+  def defragment_context(context) do
+    %TranslatorContext{free_memory_blocks: memory, var_pointer_map: var_map, pos_to_var_map: pos_map} = context
+    positions = Enum.sort(Map.keys(pos_map))
+    {defrag_code, var_map, pos_map, _target_pos} = Enum.reduce(positions, {"//Starting Defragmentation \n", var_map, pos_map, 0}, fn old_pos, {defrag_code, var_map, pos_map, target_pos} ->
+      {name, size} = Map.get(pos_map, old_pos)
+
+      if(target_pos == old_pos) do
+        {"", var_map, pos_map, target_pos + size}
+      else
+
+        var_map = Map.update!(var_map, name, fn {_pos, size} ->
+          {target_pos, size}
+        end)
+        pos_map = Map.drop(pos_map, [old_pos])
+        pos_map = Map.put(pos_map, target_pos, {name, size})
+
+        defrag_code = defrag_code <>
+          case size do
+          4 ->
+            """
+            stack_int = (int*) (stack + #{target_pos});
+            stack_int[0] = *((int*) (stack + #{old_pos}));
+            """
+          8 ->
+            """
+            stack_str = (String*) (stack + #{target_pos});
+            stack_str[0] = *((String*) (stack + #{old_pos}));
+            """
+          12 ->
+            """
+            stack_gen = (Generic*) (stack + #{target_pos});
+            stack_gen[0] = *((Generic*) (stack + #{old_pos}));
+            """
+        end
+
+        target_pos = target_pos + size
+        {defrag_code, var_map, pos_map, target_pos}
+      end
+    end)
+    memory = MemoryBlocks.defrag(memory)
+    context = %{context |
+      var_pointer_map: var_map,
+      pos_to_var_map: pos_map,
+      free_memory_blocks: memory
+    }
+    {context, defrag_code}
   end
 
   def allocate_var_in_context(context, var_name_in_c, size) do
       {memory, pos} = MemoryBlocks.get(context.free_memory_blocks, size)
       context = %{ context |
         free_memory_blocks: memory,
-        var_pointer_map: Map.put(context.var_pointer_map, var_name_in_c, {pos, size})
+        var_pointer_map: Map.put(context.var_pointer_map, var_name_in_c, {pos, size}),
+        pos_to_var_map: Map.put(context.pos_to_var_map, pos, {var_name_in_c, size})
       }
-      {context, pos}
+      {context, defrag_code, pos}
   end
 
   def return_var_in_context(context, var_name_in_c, keep \\ true) do
@@ -296,6 +354,7 @@ defmodule Honey.Compiler.Translator do
         #{code}
         bpf_printk(\"#{string}\"#{vars});
 
+        #{defrag_code}
         // Defining variable #{result_var};
         stack_int = (int*) (stack + #{pos});
         stack_int[0] = 0;
@@ -775,6 +834,9 @@ defmodule Honey.Compiler.Translator do
     {context, defrag_code} = Context.allocate_var(context, property_var, 12)
     property_pos = Context.get_var_pos(context, property_var)
     str_name_var = unique_helper_var()
+
+    var_name_in_c = var_to_string(var)
+    {access_var_pos, _size} = Map.get(context.var_pointer_map, var_name_in_c)
 
     var_name_in_c = var_to_string(var)
     {access_var_pos, _size} = Map.get(context.var_pointer_map, var_name_in_c)
