0bd1069f7e82ed53a5f37d5a2e8239c4b21d4061
ChrystianMSC <chrystianmsc@gmail.com>
2025-11-10 19:11:37 -0300

    refactor: using bpf_hash and default values.

--- examples/lib/memorymonitor.ex
+++ examples/lib/memorymonitor.ex
@@ -1,31 +1,35 @@
 defmodule MemoryMonitor do
   use Honey, license: "Dual BSD/GPL"
+  alias Honey.BpfHelpers, as: BpfHelpers
 
-  @max_entries 41943040
-  defmap(:mmap_count, :bpf_array, [max_entries: @max_entries])
-  defmap(:munmap_count, :bpf_array, [max_entries: @max_entries])
-  defmap(:brk_count, :bpf_array, [max_entries: @max_entries])
+  @max_entries 4096
+  defmap(:mmap_count, :bpf_hash, [max_entries: @max_entries])
+  defmap(:munmap_count, :bpf_hash, [max_entries: @max_entries])
+  defmap(:brk_count, :bpf_hash, [max_entries: @max_entries])
 
   @sec "tracepoint/raw_syscalls/sys_enter"
   def main(ctx) do
     syscall_id = ctx.id
-    pid = Honey.BpfHelpers.bpf_get_current_pid_tgid()
+    pid = BpfHelpers.bpf_get_current_pid_tgid()
 
     cond do
       syscall_id == 9 ->  # mmap
-        count = Honey.BpfHelpers.bpf_map_lookup_elem(:mmap_count, pid)
-        Honey.BpfHelpers.bpf_map_update_elem(:mmap_count, pid, count + 1)
-        Honey.BpfHelpers.bpf_printk(["PID: %d; MMap Operations: %llu;", pid, count + 1])
+        count = BpfHelpers.bpf_map_lookup_elem(:mmap_count, pid, 0)
+        BpfHelpers.bpf_map_update_elem(:mmap_count, pid, count + 1)
+        BpfHelpers.bpf_printk(["PID: %d; MMap Operations: %llu;", pid, count + 1])
 
       syscall_id == 11 ->  # munmap
-        count = Honey.BpfHelpers.bpf_map_lookup_elem(:munmap_count, pid)
-        Honey.BpfHelpers.bpf_map_update_elem(:munmap_count, pid, count + 1)
-        Honey.BpfHelpers.bpf_printk(["PID: %d; Munmap Operations: %llu;", pid, count + 1])
+        count = BpfHelpers.bpf_map_lookup_elem(:munmap_count, pid, 0)
+        BpfHelpers.bpf_map_update_elem(:munmap_count, pid, count + 1)
+        BpfHelpers.bpf_printk(["PID: %d; Munmap Operations: %llu;", pid, count + 1])
 
       syscall_id == 12 ->  # brk
-        count = Honey.BpfHelpers.bpf_map_lookup_elem(:brk_count, pid)
-        Honey.BpfHelpers.bpf_map_update_elem(:brk_count, pid, count + 1)
-        Honey.BpfHelpers.bpf_printk(["PID: %d; Brk Operations: %llu;", pid, count + 1])
+        count = BpfHelpers.bpf_map_lookup_elem(:brk_count, pid, 0)
+        BpfHelpers.bpf_map_update_elem(:brk_count, pid, count + 1)
+        BpfHelpers.bpf_printk(["PID: %d; Brk Operations: %llu;", pid, count + 1])
+
+      true ->
+        0
     end
 
     0
